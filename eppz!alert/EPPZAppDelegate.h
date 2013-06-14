26d1846fc28e08ac8e0aab4054704eef973d1bc9
Borbas Geri, Carnation <gergely.borbas@carnationgroup.com>
2013-06-14 17:55:40 +0200

    Initial Commit

---
+++ eppz!alert/EPPZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EPPZAppDelegate.h
+//  eppz!alert
+//
+//  Created by Carnation on 6/14/13.
+//  Copyright (c) 2013 eppz! development, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPPZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
