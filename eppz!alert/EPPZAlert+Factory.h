313eabd4cd680b36884452a629f76c5f04ec7b8e
Borbás Geri, eppz! <hello@eppz.eu>
2013-06-15 17:01:27 +0200

    1.0.2 - Added some factory pressets.

--- eppz!alert/EPPZAlert+Factory.h
+++ eppz!alert/EPPZAlert+Factory.h
@@ -1,8 +1,8 @@
 //
-//  EPPZAlert.h
+//  EPPZAlert+Factory.h
 //  eppz!tools
 //
-//  Created by Borbás Geri on 6/14/13.
+//  Created by Borbás Geri on 6/15/13.
 //  Copyright (c) 2013 eppz! development, LLC.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -10,25 +10,36 @@
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-
-#import <Foundation/Foundation.h>
-
-
-typedef void (^EPPZAlertCompletition)(NSString *selectedButtonTitle);
+#import "EPPZAlert.h"
 
 
-//EPPZAlert - A thin interface to work with.
-@interface EPPZAlert : NSObject
+typedef void (^EPPZAlertExplicitCompletition)();
 
 
+//EPPZAlert (Factory) - You can easily extend base class this way, resulting cool alert presets for your application context.
+@interface EPPZAlert (Factory)
+
+
+//Alert whithout callback.
 +(void)alertWithTitle:(NSString*) title
               message:(NSString*) message
-         buttonTitles:(NSArray*) buttonTitles
+         buttonTitles:(NSArray*) buttonTitles;
+
+//A simple "Ok" alert.
++(void)alertWithTitle:(NSString*) title
+              message:(NSString*) message;
+
+//A simple "Ok" alert (with completition).
++(void)alertWithTitle:(NSString*) title
+              message:(NSString*) message
          completition:(EPPZAlertCompletition) completition;
+
+//A simple "Yes/No/Cancel" alert (with explicit completitions).
++(void)alertWithTitle:(NSString*) title
+              message:(NSString*) message
+                  yes:(EPPZAlertExplicitCompletition) yesCompletition
+                   no:(EPPZAlertExplicitCompletition) noCompletition
+               cancel:(EPPZAlertExplicitCompletition) cancelCompletition;
 
 
 @end
-
-
-//More factory preset in a separate class.
-#import "EPPZAlert+Factory.h"
